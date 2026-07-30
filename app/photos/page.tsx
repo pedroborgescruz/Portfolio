4990d59df66274ce8c31b41408024190c052dd6b
Pedro Borges <pedroborgespc@gmail.com>
2026-07-30 00:03:04 -0300

    refactor: new data structure for photo loading

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,135 +1,9 @@
 import type { Metadata } from "next";
+import { photos } from "@/data/photos";
 
 export const metadata: Metadata = {
   title: "photos",
 };
-
-type Photo = {
-  src: string;
-  caption: React.ReactNode;
-  wide?: boolean;
-};
-
-const photos: Photo[] = [
-  {
-    src: "/assets/photography/000020940011.jpg",
-    caption: (
-      <>
-        <strong>my sister.</strong> stockholm, sweden. 2024.
-      </>
-    ),
-    wide: true,
-  },
-  {
-    src: "/assets/photography/19440001.JPG",
-    caption: (
-      <>
-        <strong>the painted ladies.</strong> san francisco, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/000020940036.jpg",
-    caption: (
-      <>
-        <strong>snowy gamla stan.</strong> stockholm, sweden. 2024.
-      </>
-    ),
-    wide: true,
-  },
-  {
-    src: "/assets/photography/19450004.JPG",
-    caption: (
-      <>
-        <strong>isa.</strong> yosemite, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/19450006.JPG",
-    caption: (
-      <>
-        <strong>trees.</strong> yosemite, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/19450012.JPG",
-    caption: (
-      <>
-        <strong>port.</strong> monterey, ca. 2024.
-      </>
-    ),
-    wide: true,
-  },
-  {
-    src: "/assets/photography/19450014.JPG",
-    caption: (
-      <>
-        <strong>monterey ice cream.</strong> monterey, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/19450018.JPG",
-    caption: (
-      <>
-        <strong>golden gate bridge.</strong> san francisco, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/19450024.JPG",
-    caption: (
-      <>
-        <strong>italian parade.</strong> san francisco, ca. 2024.
-      </>
-    ),
-    wide: true,
-  },
-  {
-    src: "/assets/photography/19450025.JPG",
-    caption: (
-      <>
-        <strong>fisherman&apos;s wharf.</strong> san francisco, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/19450031.JPG",
-    caption: (
-      <>
-        <strong>the actual painted ladies.</strong> san francisco, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/19450032.JPG",
-    caption: (
-      <>
-        <strong>chinatown.</strong> san francisco, ca. 2024.
-      </>
-    ),
-    wide: true,
-  },
-  {
-    src: "/assets/photography/19450034.JPG",
-    caption: (
-      <>
-        <strong>alamo square.</strong> san francisco, ca. 2024.
-      </>
-    ),
-  },
-  {
-    src: "/assets/photography/44800032.JPG",
-    caption: (
-      <>
-        <strong>mirror lake.</strong> yosemite, ca. 2024.
-      </>
-    ),
-    wide: true,
-  },
-];
 
 export default function PhotosPage() {
   return (
@@ -142,13 +16,20 @@ export default function PhotosPage() {
       <div className="photo-gallery">
         {photos.map((photo) => (
           <figure
-            key={photo.src}
+            key={photo.file}
             className={`photo-item tooltip-container${photo.wide ? " photo-item--wide" : ""}`}
           >
             <div className="photo-frame">
-              <img src={photo.src} alt="Photograph" />
+              <img
+                src={`/assets/photography/${photo.file}`}
+                alt={photo.title}
+              />
             </div>
-            <div className="tooltip">{photo.caption}</div>
+            {photo.title !== "untitled" ? (
+              <div className="tooltip">
+                <strong>{photo.title}.</strong> {photo.place}. {photo.year}.
+              </div>
+            ) : null}
           </figure>
         ))}
       </div>
---
+++ data/photos.ts
@@ -0,0 +1,952 @@
+export type Photo = {
+  file: string;
+  title: string;
+  place: string;
+  year: number;
+  wide?: boolean;
+};
+
+export const photos: Photo[] = [
+  {
+    file: "000020940011.jpg",
+    title: "my sister",
+    place: "stockholm, sweden",
+    year: 2024,
+    wide: true,
+  },
+  {
+    file: "19440001.JPG",
+    title: "the painted ladies",
+    place: "san francisco, ca",
+    year: 2024,
+  },
+  {
+    file: "000020940036.jpg",
+    title: "snowy gamla stan",
+    place: "stockholm, sweden",
+    year: 2024,
+    wide: true,
+  },
+  {
+    file: "19450004.JPG",
+    title: "isa",
+    place: "yosemite, ca",
+    year: 2024,
+  },
+  {
+    file: "19450006.JPG",
+    title: "trees",
+    place: "yosemite, ca",
+    year: 2024,
+  },
+  {
+    file: "19450012.JPG",
+    title: "port",
+    place: "monterey, ca",
+    year: 2024,
+    wide: true,
+  },
+  {
+    file: "19450014.JPG",
+    title: "monterey ice cream",
+    place: "monterey, ca",
+    year: 2024,
+  },
+  {
+    file: "19450018.JPG",
+    title: "golden gate bridge",
+    place: "san francisco, ca",
+    year: 2024,
+  },
+  {
+    file: "19450024.JPG",
+    title: "italian parade",
+    place: "san francisco, ca",
+    year: 2024,
+    wide: true,
+  },
+  {
+    file: "19450025.JPG",
+    title: "fisherman's wharf",
+    place: "san francisco, ca",
+    year: 2024,
+  },
+  {
+    file: "19450031.JPG",
+    title: "the actual painted ladies",
+    place: "san francisco, ca",
+    year: 2024,
+  },
+  {
+    file: "19450032.JPG",
+    title: "chinatown",
+    place: "san francisco, ca",
+    year: 2024,
+    wide: true,
+  },
+  {
+    file: "19450034.JPG",
+    title: "alamo square",
+    place: "san francisco, ca",
+    year: 2024,
+  },
+  {
+    file: "44800032.JPG",
+    title: "mirror lake",
+    place: "yosemite, ca",
+    year: 2024,
+    wide: true,
+  },
+  {
+    file: "1.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "2.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "3.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "4.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "5.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120009.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120010.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120012.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120015.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120016.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120018.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120019.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120020.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120021.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120022.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120023.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120024.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120025.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "03120026.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "11350004.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "11350005.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "11350030.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "11350036.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210001.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210003.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210012.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210013.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210020.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210021.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "15210022.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000018820004.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000018820007.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000018820010.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000018820013.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000018820014.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440004.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440005.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440007.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440008.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440010.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440011.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440017.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440022.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440033.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440034.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19440036.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19450011.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19450015.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19450022.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19450023.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19450026.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "19450033.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940001.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940004.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940006.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940007.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940022.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940023.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940025.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940026.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940029.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940030.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "000020940035.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800001.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800002.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800010.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800011.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800012.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800013.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800017.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800018.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800019.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800020.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800022.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800023.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800024.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800025.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800026.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800028.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800029.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800030.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "44800031.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490002.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490008.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490021.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490024.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490025.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490026.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490028.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490029.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490030.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490031.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490034.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490035.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "61490036.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590003.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590004.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590006.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590007.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590008.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590010.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590011.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590012.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590013.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590014.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590015.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590017.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590018.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590019.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590027.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590028.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590030.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590031.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590035.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590036.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "94590037.JPG",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "001488260009.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15521.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15522.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15526.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15527.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15541.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15542.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15548.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15551.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15563.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15568.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15571.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15572.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15573.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15574.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15575.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15580.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15581.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "scan15592.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37008.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37010.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37020.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37023.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37027.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37031.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+  {
+    file: "Scan37032.jpg",
+    title: "untitled", // TODO
+    place: "", // TODO
+    year: 0, // TODO
+  },
+];
