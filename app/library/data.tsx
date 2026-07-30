3534fd10d406a230d57d4a54e73156e1bf9bfe63
Pedro Borges <pedroborgespc@gmail.com>
2026-07-30 17:37:03 -0300

    feat: books list

---
+++ app/library/data.tsx
@@ -0,0 +1,74 @@
+export const compSciBooks = [
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "designing data-intensive applications",
+    author: "Martin Kleppmann",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "computer networks: a programmer's perspective",
+    author: "Randal E. & David R.",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "ai engineering",
+    author: "Chip Huyen",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "clean code",
+    author: "Robert C. Martin",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "deep learning",
+    author: "lan G., Yoshua B. & and Aaron C.",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "a philosophy of software design",
+    author: "John O.",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "the pragmatic programmer",
+    author: "Andrew H. & David T.",
+  },
+  {
+    tag: "compsci",
+    tagClass: "tag-comp-sci",
+    title: "fundamentals of software architecture",
+    author: "Mark R. & Neal F.",
+  },
+] as const;
+
+export const fictionBooks = [
+  {
+    tag: "fiction",
+    tagClass: "tag-fiction",
+    title: "a gentleman in moscow",
+    author: "Amor Towles",
+  },
+] as const;
+
+export const nonFictionBooks = [
+  {
+    tag: "non-fiction",
+    tagClass: "tag-nonfiction",
+    title: "tiny experiments",
+    author: "Anne Laure Le Cunff",
+  },
+  {
+    tag: "non-fiction",
+    tagClass: "tag-nonfiction",
+    title: "so good they can't ignore you",
+    author: "Cal Newport",
+  },
+] as const;
